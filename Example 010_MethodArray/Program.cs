a86d0c53e351f0f3118a1332a875488a341d062d
Larisa Slobodskaia <uvolckova156@gmail.com>
2021-11-27 20:52:42 +0100

    Example 010 MethodArray

---
+++ Example 010_MethodArray/Program.cs
@@ -0,0 +1,36 @@
+﻿// int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+// int n = array.Length;
+// int find = 18;
+
+// int index = 0;
+
+// while (index < n)
+// {
+
+//    if(array[index] == find) 
+//    {
+//        Console.WriteLine(index);
+//    }
+//     //index = index + 1;
+//     index++;   
+// }
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+   if(array[index] == find) 
+   {
+       Console.WriteLine(index);
+       break;
+   }
+    //index = index + 1;
+    index++;   
+}
